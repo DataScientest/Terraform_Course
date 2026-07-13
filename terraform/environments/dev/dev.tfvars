883b054b0539e191f4c3c046c81b30986efa5bd7
matteo fernandes <matteofernandes712@gmail.com>
2026-07-13 11:37:15 +0200

    adding TODO for chap3, and adding tfplan in .gitignore

--- terraform/environments/dev/dev.tfvars
+++ terraform/environments/dev/dev.tfvars
@@ -1,4 +1,4 @@
 project_name = "fraud-scoring"
 environment  = "dev"
 aws_region   = "eu-west-3"
-runtime_port = 8001
+runtime_port = #TODO
--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ resource "docker_container" "inference_runtime" {
 
   ports {
     internal = 8000
-    external = var.runtime_port
+    external = #TODO
   }
 
   env = [
